68beaea7ed5b0cd7e5f7eeb0fbed92e00d98fc16
4399akd <88360656+4399akd@users.noreply.github.com>
2024-10-24 16:12:03 +0800

    jk 提交了 GPIO配置的标准库和Hal库的方法

---
+++ GPIO_std/STM32f1_GPIO/User/main.c
@@ -0,0 +1,15 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "LED.h"
+
+
+int main(void)
+{
+	while(1)
+	{
+//		LED_Set(1);
+//		Delay_ms(500);
+		LED_Set(0);
+//		Delay_ms(500);
+	}
+}
